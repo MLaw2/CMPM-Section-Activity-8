953aed4dcf730cd67204bc1bffde46afd26e0b68
Michael <xxtwomadxx@gmail.com>
2023-06-01 12:08:35 -0700

    Co-authored-by: Rosiesunny <Rosiesunny@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,25 @@
+class title extends Phaser.Scene {
+   constructor() {
+      super('title');
+   }
+
+   preload() {
+
+   }
+
+   create() {
+      // this.add.text()
+   }
+}
+
+class transition extends 
+
+let config={
+    scale:{
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        mode: Phaser.Scale.FIT,
+    },
+    backgroundColor: 0x000000,
+    scene: [title, transition, leve1, level2, level3],
+    title: "Roly Poly Storyboard",
+}
